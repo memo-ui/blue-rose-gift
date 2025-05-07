5a28cb1f06483f27a9e5833b140d89fb90aaccaa
memo-ui <roxas0406@gmail.com>
2025-05-07 22:34:12 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/blue-rose-gift/', // <--- Repo-Name hier eintragen!
   plugins: [react()],
 })
